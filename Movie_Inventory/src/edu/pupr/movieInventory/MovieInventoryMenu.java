f83dd38b79abefc2cb5be6df12bd5fc764bb6e77
spacetacos1 <jemanuelle.lopez@gmail.com>
2025-10-07 16:08:06 -0400

    Restructures for Parent and Child classes

--- Movie_Inventory/src/edu/pupr/movieInventory/MovieInventoryMenu.java
+++ Movie_Inventory/src/edu/pupr/movieInventory/MovieInventoryMenu.java
@@ -7,7 +7,21 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class MovieInventory extends JFrame {
+
+//Parent Class
+class Movie {
+	private String title;
+}
+
+
+//Child Class
+class MovieInventory extends Movie {
+	
+}
+
+
+
+public class MovieInventoryMenu extends JFrame {
 
 	private JPanel contentPane;
 
@@ -18,7 +32,7 @@ public class MovieInventory extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MovieInventory frame = new MovieInventory();
+					MovieInventoryMenu frame = new MovieInventoryMenu();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +44,7 @@ public class MovieInventory extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MovieInventory() {
+	public MovieInventoryMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
